5f528779a7cad247086c9935342cf57bcfe017eb
PvTiep <0.199qui@gmail.com>
2023-03-05 15:56:29 +0700

    tiep tiep

--- js/login.js
+++ js/login.js
@@ -15,8 +15,9 @@ loginButton.addEventListener('click', function() {
                 
                 setLoginStatus(true);
                 storeUser(result.id, result.userName, result.phone, result.address, result.idAccount);
-                var users = localStorage.getItem('userId');
+                // users = localStorage.getItem('userId');
                 //alert(users);
+                window.location.href = "/index.html";
                 
             } else {
                 localStorage.clear();
