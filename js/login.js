c91250d915ae5fc10b7fbd58baf94e34003755ed
NguyenQuy <darklord0305@gmail.com>
2023-03-13 10:33:55 +0700

    code login page with jwt

--- js/login.js
+++ js/login.js
@@ -1,44 +1,29 @@
+(function ($) {
+    'use strict';
+    const inputEmail = document.getElementById('emailLogin');
+    const inputPass = document.getElementById('passwordLogin');
+    $(document).on("click", "#loginbtn", function() {
+        var emailUser = inputEmail.value;
+        var pass = inputPass.value;
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/accounts/login",
+            contentType: "application/json",
+            data: JSON.stringify({ email: emailUser, accountPassword: pass }),
+            success: function (result, status, xhr) {
+                document.cookie = "token=" + result + ";path=/;"
+                console.log(result);
+                testcookie();
+            },
+            error: function (xhr, status, error) {
+                alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    })
 
-const loginButton = document.getElementById('loginbtn');
-const inputEmail = document.getElementById('emailLogin');
-const inputPass = document.getElementById('passwordLogin');
-loginButton.addEventListener('click', function() {
-    var emailUser = inputEmail.value;
-    var pass = inputPass.value;
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/accounts/login",
-        contentType: "application/json",
-        data: JSON.stringify({email: emailUser, accountPassword: pass}),
-        success: function (result, status, xhr) {
-            // if(status = 'success') {
-                
-                setLoginStatus(true);
-                storeUser(result.id, result.userName, result.phone, result.address, result.idAccount);
-                // users = localStorage.getItem('userId');
-                //alert(users);
-                window.location.href = "/index.html";
-                
-            // } else {
-            //     localStorage.clear();
-            // }
-            console.log(result);
-        },
-        error: function (xhr, status, error) {
-            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-        }
-    });
-});
-
-function setLoginStatus(isLoggedIn) {
-    localStorage.setItem("isLoggedIn", isLoggedIn);
-}
-
-function storeUser(id, userName, phone, address, idAccount) {
-    localStorage.setItem("userId", id);
-    localStorage.setItem("userName", userName);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("address", address);
-    localStorage.setItem("idAccount", idAccount);
-}
-
+    function testcookie() {
+        var jwt = document.cookie.split(";").find(row => row .startsWith('token=')).split('=')[1];
+        const decoded = jwt_decode(jwt);
+        console.log(decoded.sub);
+    }
+})(jQuery);
